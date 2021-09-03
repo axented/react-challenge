4ebb06f7d778da6b95b2f382e6a3edb095e69454
Bernardo Garza <bernardo.g.landa@gmail.com>
2021-09-03 10:08:39 -0500

    Changed folder structure and created page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MessageList from '../../components/MessageList/MessageList';
+
+const Home = () => {
+  return (
+    <div>
+      <MessageList />
+    </div>
+  );
+};
+
+export default Home;
