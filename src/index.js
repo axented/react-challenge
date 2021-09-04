491118036b50334c3b27bc78915fc70a0cbd7f61
Bernardo Garza <bernardo.g.landa@gmail.com>
2021-09-03 19:08:58 -0500

    Added provider to main component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MessagesContextProvider from './context/MessagesContext';
 import Home from './pages/Home/Home';
 
 const NewApp = require('./pages/Home/Home.jsx').default;
 
 function renderApp(App) {
-  ReactDOM.render(<App />, document.getElementById('root'));
+  ReactDOM.render(
+    <MessagesContextProvider>
+      <App />
+    </MessagesContextProvider>,
+    document.getElementById('root'),
+  );
 }
 
 renderApp(Home);
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,12 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import MessageList from '../../components/MessageList/MessageList';
 import TestComponent from '../../components/TestComponent/TestComponent';
-import MessagesContextProvider from '../../context/MessagesContext';
 
 const Home = () => {
   return (
     <div>
-      <MessagesContextProvider>
-        <MessageList />
-        <TestComponent />
-      </MessagesContextProvider>
+      <MessageList />
+      <TestComponent />
     </div>
   );
 };
