139dc5c6869d97850031c40687cade9f15663ef0
Bernardo Garza <bernardo.g.landa@gmail.com>
2021-09-04 16:21:03 -0500

    Notifications working

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,11 +1,37 @@
 import React, { useEffect, useState } from 'react';
 
 const Notification = ({ data }) => {
-  return (
-    <div>
-      <p>snackbar</p>
-    </div>
-  );
+  const [queue, setQueue] = useState([]);
+
+  useEffect(() => {
+    if (data[0] !== undefined) setQueue((queue) => [...queue, data[0].message]);
+  }, [data]);
+
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      if (queue.length) {
+        setQueue((queue) => queue.slice(1));
+      }
+    }, 2000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [queue]);
+
+  const handleClick = () => {
+    setQueue((queue) => queue.slice(1));
+  };
+
+  if (queue.length) {
+    return (
+      <div>
+        <p>{queue[0]}</p>
+        <p onClick={handleClick}>&#10005;</p>
+      </div>
+    );
+  } else {
+    return false;
+  }
 };
 
 export default Notification;
